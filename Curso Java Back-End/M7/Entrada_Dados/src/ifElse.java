06a50657cf205e48e7ff69e609a44aee17c2ef33
Dirceu Maciel Torres <maciel.dirceu@hotmail.com>
2024-01-03 23:16:58 -0300

    ifelse

--- Curso Java Back-End/M7/Entrada_Dados/src/ifElse.java
+++ Curso Java Back-End/M7/Entrada_Dados/src/ifElse.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
 import java.util.Locale;
 public class ifElse {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
 
+        System.out.println("DIGITE A QUANTIDADE DE MINUTOS ULTILIZADAS: ");
+        int num = scan.nextInt();
+        double juros = (double)0.5*num;
+        if(num <= 100){
+            System.out.println("O VALOR DE SUA FATURA É R$ 50.00");
+              }
+        else{
+            System.out.println("O VALOR DE SUA FATURA É: R$ "+juros + "Reais");
+        }
+
+        scan.close();
+    }
 }
