c36eb7896da278efa1105e2878f8b098445ef24c
Dirceu Maciel Torres <maciel.dirceu@hotmail.com>
2024-03-26 21:06:48 -0300

    exercicios1_26_03

--- Arrays3/src/Main.java
+++ Arrays3/src/Main.java
@@ -8,13 +8,26 @@ public class Main {
 
 		turma1.alunos[0] = new Aluno();
 		turma1.alunos[0].nome = "Antonio";
+		turma1.alunos[0].idade = 3;
 
 		turma1.alunos[1] = new Aluno();
 		turma1.alunos[1].nome = "Maria";
+		turma1.alunos[1].idade = 4;
 
 		turma1.alunos[2]= new Aluno();
 		turma1.alunos[2].nome = "Joana";
+		turma1.alunos[2].idade = 4;
 
-		System.out.println();
+		/*for (int i = 0; i < turma1.alunos.length; i++) {
+			if (turma1.alunos[i] != null) {
+				System.out.printf("%d, %s, %d anos %n", i, turma1.alunos[i].nome, turma1.alunos[i].idade);
+			}
+		}
+		for (int i = 0; i < turma1.alunos.length; i++) {
+			Aluno aluno = turma1.alunos[i];
+			if (turma1.alunos[i] != null) {
+				System.out.printf("%d, %s, %d anos %n", i, turma1.alunos[i].nome, turma1.alunos[i].idade);*/
+		turma1.imprimirListaAlunos();
 	}
 }
+
--- Arrays3/src/Turmas.java
+++ Arrays3/src/Turmas.java
@@ -3,4 +3,12 @@ public class Turmas {
 	String idTurma;
 	String nomeProfessora;
 	Aluno[] alunos;
+	void imprimirListaAlunos(){
+		for (Aluno aluno : alunos) {
+			if (aluno != null){
+				System.out.printf("%s, %d anos %n", aluno.nome, aluno.idade);
+			}
+
+		}
+	}
 }
